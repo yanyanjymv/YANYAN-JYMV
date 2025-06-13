3579338b22394e0e9b14e76d5d48eac7d87373e8
yanyanjymv <yanyanjulyandi123@gmail.com>
2025-06-13 13:41:38 +0000

    abc

--- html/index_mobil.php
+++ html/index_mobil.php
@@ -12,7 +12,7 @@
 	<head> 
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial scale=0.1" />
-		<title> Alimrugi | Online shop for automobiles </title>
+		<title> Alimrugi | Online shop for automobilese </title>
 
 	
 	<link rel="stylesheet" href="css/custom.css">
