57eae2758c1a13b4cf1dde1e5d0888e02fc67941
yanyanjymv <yanyanjulyandi123@gmail.com>
2025-06-15 04:41:43 +0000

    a24

--- Dockerfile.php
+++ Dockerfile.php
@@ -6,8 +6,5 @@ RUN docker-php-ext-install mysqli pdo pdo_mysql json
 # Copy custom php.ini file
 COPY custom-php.ini /usr/local/etc/php/conf.d/
 
-# Set working directory
-WORKDIR /var/www
-
 # Start php-fpm
 CMD ["php-fpm"]
