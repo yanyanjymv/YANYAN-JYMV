198c83d40dd022ea821081e87de854c4f8926fd3
yanyanjymv <yanyanjulyandi123@gmail.com>
2025-06-20 13:06:39 +0000

    test1

--- php/html/index.php
+++ php/html/index.php
@@ -1,120 +1,82 @@
-<?php
-    include 'koneksi.php';
-    session_start();
-    if(isset($_SESSION['roles']) == null){
-?>
+<?php 
+include("login_connection.php");
+session_start();
 
-<html>
-<head> 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Alimrugi | Online shop for automobile</title>
+$username = "";
+$password = "";
+$err = "";
 
-    <link rel="stylesheet" href="css/custom.css">
-    <link rel="stylesheet" href="css/custom2.css">
-    <link rel="stylesheet" href="css/font-awesome/css/font-awesome.min.css">
-</head>
-
-<body>
-
-    <div id="header">
-        <div class="center">
-            <!-- Logo -->
-            <div class="logo">
-                <a href="index.php">
-    					<img src="img/alimrugi.jpg" alt="Logo Alimrugi">
-  					</a>
-            </div>
-            <!-- Logo -->
-
-            <!-- Navbar -->
-            <div class="navbar">
-                <ul>
-                    <li> <a href="login.php"> Login </a></li>
-                </ul>
-            </div> 
-            <div class="clear"></div>
-        </div>
-    </div>
-
-    <!-- SLIDER -->
-    <div id="slider">
-        <div class="center">
-            <h1> Selamat Datang di Alim Rugi </h1>
-            <h2> Jual Beli Mobil Terbaik Secara Online</h2>
-            <br>
-            <h3> Untuk Memulai Silahkan Mendaftar Terlebih Dahulu </h3>
-            <a href="signup.php" class="btn-primary"> REGISTRASI </a>
-        </div>
-    </div>
-    <!-- END SLIDER -->     
-
-    <div class="center">
-        <div id="menumobil">
-            <form action="" method="post">
-                <input type="search" name="search" class="search" placeholder="Masukkan nama mobil">
-                <input type="submit" name="submit" class="search btn-cari" value="Cari"> 
-            </form>
-            <div class="judulmenu"> Menu Mobil </div>    
-        </div>
-
-        <br>
-
-        <?php 
-            $submit = @$_POST['submit'];
-            $search = @$_POST['search'];
-
-            if($submit && $search != ""){
-                $query = mysqli_query($konekdb, "SELECT * FROM tb_mobil WHERE nm_mobil LIKE '%$search%' ORDER BY id_mobil ASC");
-            } else {
-                $query = mysqli_query($konekdb, "SELECT * FROM tb_mobil ORDER BY id_mobil ASC"); // Menampilkan semua data
-            }
-
-            $cek = mysqli_num_rows($query);
-            if($cek < 1){
-                echo "Data yang anda cari tidak ditemukan!";
-            }
-
-            while($data = mysqli_fetch_array($query)){
-        ?>
-
-        <div class="kontenspan">
-            <div class="table-responsive">
-                <table class="tableisi">
-                    <tr> <th> <?php echo $data['nm_mobil']; ?> </th> </tr>    
-                    <tr> 
-                        <td> 
-                            <img class="spangambar" src="img/mobil/<?php echo $data['gambar']; ?>" alt="Foto"> 
-                        </td> 
-                    </tr>
-                    <tr> <td class="tdspek"> <?php echo $data['spek_mobil']; ?> </td> </tr>
-                    <tr> <td class="tdharga"> RP.<?php echo number_format($data['harga_mobil'],0,",","."); ?> </td> </tr>
-                </table>
-            </div>
-        </div>
-
-        <?php } ?>
-        
-        <div class="clear"></div>
-
-        <div class="copyright">
-            Copyright &copy; by KELOMPOK 2 
-        </div>
-    </div> <!-- PENUTUP ISI-->
-
-</body>
-
-</html>
-
-<?php
-    } else {
-        if($_SESSION['roles'] == "admin"){
-            header("location:assets/admin/index.php");
-        } elseif($_SESSION['roles'] == "user"){    
-            header("location:assets/users/index.php");
-        } else {
-            echo "User tidak ditemukan";
-            session_destroy();
-        }
+if (isset($_POST['login'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    
+    if ($username == '' || $password == '') {
+        $err .= "<li>Both username and password are required.</li>";
     }
+
+    if (empty($err)) {
+        $stmt = $koneksi->prepare("SELECT * FROM admin WHERE username = ?");
+        $stmt->bind_param("s", $username);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($result->num_rows > 0) {
+            $user = $result->fetch_assoc();
+
+            if (md5($password) === $user['password']) {
+                $_SESSION['admin_username'] = $username;
+                $_SESSION['level'] = $user['level'];
+                $_SESSION['departement'] = $user['departement'];
+                
+                if ($user['level'] === "admin") {
+                    header("Location: home_hrd.php");
+                } elseif ($user['level'] === "spv" && $user['departement'] === "IT") {
+                    header("Location: home_spv_it.php");
+                } elseif ($user['level'] === "karyawan") {
+                    header("Location: home_karyawan.php");
+                } elseif ($user['level'] === "spv" && $user['departement'] === "MARKETING") {
+                    header("Location: home_spv_marketing.php");
+                }
+                exit();
+            } else {
+                $err .= "<li>Invalid username or password</li>";
+            }
+        } else {
+            $err .= "<li>Invalid username or password</li>";
+        }
+
+        $stmt->close();
+    }
+}
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+<div class="container">
+    <div class="box form-box">
+        <header>Login</header>
+        <?php if($err): ?>
+            <ul class="error-message"><?php echo $err; ?></ul>
+        <?php endif; ?>
+        <form action="" method="post">
+            <div class="field input">
+                <input type="text" name="username" placeholder="username" required>
+            </div>
+            <div class="field input">
+                <input type="password" name="password" placeholder="password" required>
+            </div>  
+            <div class="field"> 
+                <input type="submit" class="btn" name="login" value="LOGIN">
+            </div>
+        </form>
+    </div>        
+</div>   
+</body>
+</html>
